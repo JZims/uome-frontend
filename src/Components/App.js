23ded737a350f405ae3edaee76d02c8727b0cd8a
Adam Schwartz <adamberard24@gmail.com>
2021-04-19 14:16:18 -0400

    Switching drivers

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Login from "./Login"
 import GroupsPage from "./GroupPage"
 import CreateNewGroup from "./CreateNewGroup"
 
 function App() {
+const [groups, setGroups] = useState([])
+const [userLogin, setUserLogin] = useState("")
+
+
+
+
+
+function handleSubmit(e) {
+  e.preventDefault()
+  
+  setUserLogin(e.target.value)
+  console.log(userLogin)
+
+  fetch(`http://localhost:3000/user?_embed=groups&name=${userLogin}`)
+.then((r) => r.json())
+.then(data => setGroups(data))
+
+
+}
+
   return (
     <div className="App">
-      <Login />
-      <GroupsPage />
+      <Login setLogin={setUserLogin}
+      login={userLogin}
+      onHandleSubmit={handleSubmit}/>
+      
+      
+      <GroupsPage groups={groups} />
       <CreateNewGroup />
     </div>
   );
--- src/Components/GroupCard.js
+++ src/Components/GroupCard.js
@@ -1,14 +1,21 @@
 import React from "react"
 import BalancesGraph from "./BalancesGraph"
 
-function GroupCard(){
+function GroupCard({name, groups}){
 
+const individualGroups = groups.map((groupObj) => {
 
+    return (<li>{groupObj.name}</li>)
+})
     return (
-    <div>
+    <ul>
         Group Card
+        <h3>{name}</h3>
+        <div>{individualGroups}</div>
+        
         <BalancesGraph/>
-    </div>
+
+    </ul>
     )
 }
 
